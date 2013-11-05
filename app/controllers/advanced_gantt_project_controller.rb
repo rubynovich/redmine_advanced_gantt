011a9aed74ddc80f951ff4ef9f62e5e83bd01538
Maksim Koritskiy <m.koritskiy@gorkapstroy.ru>
2013-11-05 16:58:07 +0400

    fix avatar

--- app/controllers/advanced_gantt_project_controller.rb
+++ app/controllers/advanced_gantt_project_controller.rb
@@ -108,7 +108,7 @@ class AdvancedGanttProjectController < ApplicationController
     @issue_ancestors = []
     add_version = "v#{options[version.id]}" if options[:version]
     issues.each do |issue|
-      assign_avatar = "/people/avatar?id=#{issue.assigned_to.avatar.id}&size=100".html_safe if issue.assigned_to && issue.assigned_to.avatar
+      assign_avatar = "/people/avatar?id=#{issue.assigned_to.avatar.id}&size=100".html_safe if issue.assigned_to && issue.assigned_to.try(:avatar)
       item = {
           id: "i#{issue.id}#{add_version}",
           priority: issue.level+issue.project.level+1,
